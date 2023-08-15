eaa33494992ea5fdf4569ac1776eb4a13a0c50b2
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-08-15 09:14:05 -0700

    Update servertest.py

--- servertest.py
+++ servertest.py
@@ -13,7 +13,7 @@ parameter_value = "230524-0173"
 
 
 def getBinddes(input):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
 
@@ -27,7 +27,7 @@ def getBinddes(input):
     return partNameDf
 
 def getPartsPrice(partInfoDf):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     
@@ -58,7 +58,7 @@ def getPartsPrice(partInfoDf):
     return pricingDf
 
 def getAllPrice(ticketN):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
 
@@ -91,7 +91,7 @@ def getAllPrice(ticketN):
     conn.close()
     return ticketDf, LRatesDf, TRatesDf, misc_ops_df
 def getDesc(ticket):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     select_query = "SELECT * FROM [CF_Universal_workdescription_insert] WHERE TicketID = ?"
@@ -103,7 +103,7 @@ def getDesc(ticket):
         return "None", 1, "None"
     return dataset[0][1], dataset[0][2], dataset[0][3]
 def getAllTicket(ticket):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
 
@@ -148,7 +148,7 @@ def getAllTicket(ticket):
     return ticketLaborDf, ticketTripDf, ticketPartsDf, ticketMiscDf, ticketMaterialsDf, ticketSubDf
 
 def updateAll(ticket, desc, editable, NTE_Quote, laborDf,  tripDf, partsDf, miscDf, materialDf, subDf):
-    conn_str = f"DRIVER={{SQLaddress}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
+    conn_str = f"DRIVER={SQLaddress};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;"
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
     
