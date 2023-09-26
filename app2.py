ce46512b3fb6959f4031af0ac2209a0fbf70c42d
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-09-26 15:49:42 -0400

    Update app2.py
    Signed-off-by: Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>

--- app2.py
+++ app2.py
@@ -134,6 +134,9 @@ def mainPage():
                 st.session_state.editable = int(parentDf["Editable"])
             else:
                 st.session_state.editable = 1
+            if parentDf["Status"].get(0) is not None and (parentDf["Status"].get(0) == " Approved" or parentDf["Status"].get(0) == " Processed"):
+                st.error("this ticket is now in GP")
+                st.session_state.editable = 0
             left_data = {
                     'To': st.session_state.ticketDf['CUST_NAME'] + " " + st.session_state.ticketDf['CUST_ADDRESS1'] + " " +
                         st.session_state.ticketDf['CUST_ADDRESS2'] + " " + st.session_state.ticketDf['CUST_ADDRESS3'] + " " +
@@ -911,26 +914,26 @@ def mainPage():
                     st.success("Successfully updated to database!")                
                     # submitQuotes(pdf_base64)
                     # st.experimental_rerun()
-                    incol1, incol2, incol3 = st.columns([1,1,1])
-                    with incol1:
-                        if st.button(str(st.session_state.NTE_Quote)+" Approve", key="3"):
-                            approvetime = datetime.now()
-                            approve = approvetime.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-                            st.session_state.editable = 0
-                            updateParent(st.session_state.ticketN, st.session_state.editable, st.session_state.NTE_Quote, "1900-01-01 00:00:00.000", approve,  "1900-01-01 00:00:00.000", st.session_state.ticketDf["BranchName"].get(0), "approve")
-                            updateAll(st.session_state.ticketN, str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, 
-                                st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df)
-                            st.success("Successfully updated to database!")
-                            refresh()
-                    with incol2:
-                        if st.button(str(st.session_state.NTE_Quote)+"\nDecline", key="4"):
-                            declinetime = datetime.now()
-                            decline = declinetime.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-                            updateParent(st.session_state.ticketN, 1, st.session_state.NTE_Quote, "1900-01-01 00:00:00.000",  "1900-01-01 00:00:00.000", decline, st.session_state.ticketDf["BranchName"].get(0), "decline")
-                            updateAll(st.session_state.ticketN, str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, 
-                                st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df)
-                            st.success("Successfully updated to database!")
-                            refresh()
+                incol1, incol2, incol3 = st.columns([1,1,1])
+                with incol1:
+                    if st.button(str(st.session_state.NTE_Quote)+" Approve", key="3"):
+                        approvetime = datetime.now()
+                        approve = approvetime.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                        st.session_state.editable = 0
+                        updateParent(st.session_state.ticketN, st.session_state.editable, st.session_state.NTE_Quote, "1900-01-01 00:00:00.000", approve,  "1900-01-01 00:00:00.000", st.session_state.ticketDf["BranchName"].get(0), "approve")
+                        updateAll(st.session_state.ticketN, str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, 
+                            st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df)
+                        st.success("Successfully updated to database!")
+                        refresh()
+                with incol2:
+                    if st.button(str(st.session_state.NTE_Quote)+"\nDecline", key="4"):
+                        declinetime = datetime.now()
+                        decline = declinetime.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                        updateParent(st.session_state.ticketN, 1, st.session_state.NTE_Quote, "1900-01-01 00:00:00.000",  "1900-01-01 00:00:00.000", decline, st.session_state.ticketDf["BranchName"].get(0), "decline")
+                        updateAll(st.session_state.ticketN, str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, 
+                            st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df)
+                        st.success("Successfully updated to database!")
+                        refresh()
                 incol1, incol2, incol3 = st.columns([1,1,1])
             category_table_data = []
             for category in categories:
