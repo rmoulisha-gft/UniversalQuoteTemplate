b95f680368021ccc08a71a85b12c951494aa9809
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-10-02 22:13:27 -0400

    Update app2.py
    Signed-off-by: Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>

--- app2.py
+++ app2.py
@@ -1209,7 +1209,7 @@ def mainPage():
                 st.write(payload)
                 checkout(st.session_state.ticketDf['Purchase_Order'].values[0])
                 submitFmQuotes(pdf_base64, st.session_state.ticketDf['Purchase_Order'].values[0], str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df, total_price, total_price_with_tax)
-                st.experimental_rerun()
+                # st.experimental_rerun()
 
         if(len(st.session_state.ticketDf)!=0 and st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "CIR0001"):
             if st.sidebar.button("Submit to CircleK"):
