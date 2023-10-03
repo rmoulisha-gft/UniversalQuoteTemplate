d21f37b763c3df713874b5a102353223346aca3a
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2023-10-02 21:45:32 -0400

    Update app2.py
    Signed-off-by: Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>

--- app2.py
+++ app2.py
@@ -1174,17 +1174,18 @@ def mainPage():
                     st.markdown(pdf_display, unsafe_allow_html=True)
                     st.download_button("Download PDF", merged_buffer, file_name=f'{st.session_state.ticketN}-quote.pdf', mime='application/pdf')
                     
-        if st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "MAJ0001":
+        if len(st.session_state.ticketDf)!=0 and st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "MAJ0001":
             if st.sidebar.button("Submit to FMDash"):
+                checkout(st.session_state.ticketDf['Purchase_Order'].values[0])
                 submitFmQuotes(pdf_base64, st.session_state.ticketDf['Purchase_Order'].values[0], str(st.session_state.workDesDf["Incurred"].get(0)), str(st.session_state.workDesDf["Proposed"].get(0)), st.session_state.labor_df, st.session_state.trip_charge_df, st.session_state.parts_df, st.session_state.miscellaneous_charges_df, st.session_state.materials_and_rentals_df, st.session_state.subcontractor_df, total_price, total_price_with_tax)
                 st.experimental_rerun()
 
-        if(st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "CIR0001"):
+        if(len(st.session_state.ticketDf)!=0 and st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "CIR0001"):
             if st.sidebar.button("Submit to CircleK"):
                 wo_cost_information(category_totals["Labor"], category_totals["Trip Charge"], category_totals["Parts"], category_totals["Miscellaneous Charges"], category_totals["Materials and Rentals"], category_totals["Subcontractor"], taxRate, st.session_state.ticketDf['Purchase_Order'])
                 st.experimental_rerun()
         
-        if(st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "MUR0001"):
+        if(len(st.session_state.ticketDf)!=0 and st.session_state.ticketDf['LOC_CUSTNMBR'].get(0) == "MUR0001"):
             if st.sidebar.button("Submit to Verisae"):
                 submitQuoteVerisae(st.session_state.ticketDf['CUST_NAME'].get(0), st.session_state.ticketN, str(st.session_state.workDesDf["Incurred"].get(0)) + str(st.session_state.workDesDf["Proposed"].get(0)), category_totals["Trip Charge"], category_totals["Parts"], category_totals["Labor"], category_totals["Miscellaneous Charges"], taxRate, st.session_state.ticketDf['Purchase_Order'])
                 st.experimental_rerun()
