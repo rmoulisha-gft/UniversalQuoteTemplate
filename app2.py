523f77b4ad882c98595cc95b7fe9c1853eeec370
Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>
2024-07-29 20:58:57 +0800

    Update app2.py
    Signed-off-by: Charlieletscode <37406313+Charlieletscode@users.noreply.github.com>

--- app2.py
+++ app2.py
@@ -443,7 +443,7 @@ def mainPage():
                                                 "Description",
                                                 help="Description",
                                                 width=inwidth/4,
-                                                # options=concatenated_values,
+                                                options=concatenated_values,
                                             ),
                                             "UNIT Price": st.column_config.NumberColumn(
                                                 "UNIT Price",
